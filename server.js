c9d020e8cacb68e694acd3f70e99fcc0e69d714c
Siril C Antony <sirilcantony@gmail.com>
2024-05-03 22:06:13 +0530

    changed api key may change later

--- server.js
+++ server.js
@@ -5,34 +5,34 @@ import cors from "cors"
 import Replicate from "replicate";
 import mongoose from "mongoose";
 import axios from "axios";
-const app=Express()
+const app = Express()
 app.use(cors())
 app.use(bodyParser.json())
 
 mongoose.connect("mongodb+srv://aiproject:9L3PJ3WnAplsWTjw@cluster0.pbkf96c.mongodb.net/AI_images?retryWrites=true&w=majority")
-.then(()=>console.log("mongo atlas connected"))
-.catch(()=>console.log(err))
+    .then(() => console.log("mongo atlas connected"))
+    .catch(() => console.log(err))
 
 // mongoose.connect("mongodb://localhost/newform")
 // .then(()=>console.log("mongo connected"))
 // .catch(()=>console.log(err))
 
-const datachema= new mongoose.Schema({
-    image_data:String,
-    iname:String
+const datachema = new mongoose.Schema({
+    image_data: String,
+    iname: String
 })
 
-const mon = mongoose.model('textimage',datachema)
+const mon = mongoose.model('textimage', datachema)
 
-const texttoimg= async(req,res,next)=>{
+const texttoimg = async (req, res, next) => {
     const { question } = req.body
     console.log(question)
     const replicate = new Replicate({
-        auth: "r8_OdCuHKrRXdWMheUrW5YlO6iyQbmkc640oFA1Q",
+        auth: "r8_Sg7YJKmROQ2gS7hznNx9Wtr0FbDL7dd3OiV8i",
     });
 
     const output = await replicate.run(
-        "playgroundai/playground-v2-1024px-aesthetic:42fe626e41cc811eaf02c94b892774839268ce1994ea778eba97103fe1ef51b8",
+        "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
         {
             input: {
                 width: 600,
@@ -47,27 +47,27 @@ const texttoimg= async(req,res,next)=>{
         }
     );
     console.log(output);
-    req.textvalue =output
+    req.textvalue = output
     next()
 }
 app.post('/saveimage', texttoimg, async (req, res) => {
     const textval = req.textvalue
     const { question } = req.body
-    await axios.get(textval[0],{responseType:"arraybuffer"}).then(async response=>{
-        const bufferdata=response.data
-        const base64data=bufferdata.toString("base64")
-        const savedata_url ="data:image/jpeg;base64,"+base64data
-        const sdata=new mon({image_data:savedata_url,iname:question})
+    await axios.get(textval[0], { responseType: "arraybuffer" }).then(async response => {
+        const bufferdata = response.data
+        const base64data = bufferdata.toString("base64")
+        const savedata_url = "data:image/jpeg;base64," + base64data
+        const sdata = new mon({ image_data: savedata_url, iname: question })
         await sdata.save()
         res.send(textval)
     })
 })
 
-app.get('/images',async (req,res)=>{
-    const sdata= await mon.find()
+app.get('/images', async (req, res) => {
+    const sdata = await mon.find()
     // console.log(sdata)
     res.json(sdata)
-} )
+})
 
 
-app.listen(5000,()=>{console.log("server started")})
+app.listen(5000, () => { console.log("server started") })
